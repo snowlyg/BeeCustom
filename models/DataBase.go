2f2e9150f31c7b99338fc62c7c48cc3ff0b77d0e
luodan@elinkport.com <linximeng>
2019-11-19 17:30:47 +0800

    使用 cansbin 做权限控制

--- models/DataBase.go
+++ models/DataBase.go
@@ -1,34 +1,65 @@
 package models
 
 import (
-	"bufio"
-	"io"
-	"os"
+	"bytes"
+	"fmt"
+	"os/exec"
 
-	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego"
 )
 
 func DataReset() (bool, error) {
-	f, err := os.Open("bee_custom.sql")
+
+	dbType := beego.AppConfig.DefaultString("db_type", "mysql")
+	//数据库名称
+	//dbName := beego.AppConfig.DefaultString(dbType+"::db_name", "bee_custom")
+	//数据库连接用户名
+	dbUser := beego.AppConfig.DefaultString(dbType+"::db_user", "root")
+	//数据库连接用户名
+	dbPwd := beego.AppConfig.DefaultString(dbType+"::db_pwd", "")
+	//数据库IP（域名）
+	dbHost := beego.AppConfig.DefaultString(dbType+"::db_host", "127.0.0.1")
+	//数据库端口
+	dbPort := beego.AppConfig.DefaultString(dbType+"::db_port", "3306")
+
+	arv := []string{fmt.Sprintf("--host=%s", dbHost), fmt.Sprintf("--port=%d", dbPort), fmt.Sprintf("-u%s", dbUser), fmt.Sprintf("-p%s", dbPwd), ">", "bee_custom.sql"}
+
+	cmd := exec.Command("mysql ", arv...)
+
+	var out bytes.Buffer
+
+	cmd.Stdout = &out
+
+	err := cmd.Run()
+
 	if err != nil {
-		return false, err
+
+		fmt.Println(err)
 	}
 
-	defer f.Close()
+	fmt.Printf(out.String())
 
-	o := orm.NewOrm()
-	buf := bufio.NewReader(f)
-	for {
-		line, err := buf.ReadString(';')
-		if err != nil {
-			if err == io.EOF {
-				return true, nil
-			}
-			return false, err
-		}
-		_, err = o.Raw(line).Exec()
-		if err != nil {
-			return false, err
-		}
-	}
+	//f, err := os.Open("bee_custom.sql")
+	//if err != nil {
+	//	return false, err
+	//}
+	//
+	//defer f.Close()
+	//
+	//o := orm.NewOrm()
+	//buf := bufio.NewReader(f)
+	//for {
+	//	line, err := buf.ReadString(';')
+	//	if err != nil {
+	//		if err == io.EOF {
+	//			return true, nil
+	//		}
+	//		return false, err
+	//	}
+	//	_, err = o.Raw(line).Exec()
+	//	if err != nil {
+	//		return false, err
+	//	}
+	//}
+
 }
