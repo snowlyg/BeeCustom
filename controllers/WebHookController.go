e8b2d22e38ed75710576d5c34d07df447186b145
569616226@qq.com <linximeng990623>
2019-12-04 20:41:26 +0800

    货物申报

--- controllers/WebHookController.go
+++ controllers/WebHookController.go
@@ -29,15 +29,15 @@ func (c *WebHookController) Get() {
 
 	if calculateSignature == signature {
 		dbType := beego.AppConfig.String("db_type")
-		//数据库名称
+		// 数据库名称
 		dbName := beego.AppConfig.String(dbType + "::db_name")
-		//数据库连接用户名
+		// 数据库连接用户名
 		dbUser := beego.AppConfig.String(dbType + "::db_user")
-		//数据库连接用户名
+		// 数据库连接用户名
 		dbPwd := beego.AppConfig.String(dbType + "::db_pwd")
-		//数据库IP（域名）
+		// 数据库IP（域名）
 		dbHost := beego.AppConfig.String(dbType + "::db_host")
-		//数据库端口
+		// 数据库端口
 		dbPort := beego.AppConfig.String(dbType + "::db_port")
 		arv := []string{"-driver=mysql", fmt.Sprintf(`-conn="%s:%s@tcp(%s:%s)/%s"`, dbUser, dbPwd, dbHost, dbPort, dbName)}
 		enums.Cmd("cd", []string{"/root/go/src/BeeCustom"})
