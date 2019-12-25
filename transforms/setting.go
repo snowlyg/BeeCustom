3a626025ad0950f0b0aaac27b28b694bfd9623e6
luodan@elinkport.com <linximeng>
2019-12-25 12:39:29 +0800

    完善数据格式化

--- transforms/setting.go
+++ transforms/setting.go
@@ -16,7 +16,7 @@ func (s *Setting) GetValueEnd(v string) string {
 	value := html.UnescapeString(v)
 	valueEnd := value[:len(value)-1]
 	if len(value) > 30 {
-		valueEnd = value[:30] + "..."
+		valueEnd = value[:30]
 	}
 
 	return valueEnd
