f37410dc26f20d25edf72556111b43493cfd92c9
snowly <BrendenAudrina6287@gmail.com>
2020-01-14 00:32:11 +0800

    更新自动更新

--- controllers/WebHookController.go
+++ controllers/WebHookController.go
@@ -26,7 +26,6 @@ func (c *WebHookController) Get() {
 	sha1 := enums.Hmac(SECRETTOKEN, res)
 	calculateSignature := "sha1=" + sha1
 
-	utils.LogDebug(fmt.Sprintf("calculateSignature == signature:%v-%v", calculateSignature == signature, res))
 	if calculateSignature == signature {
 		enums.Cmd("cd", "", []string{"/root/go/src/BeeCustom"})
 		enums.Cmd("git", "", []string{"pull"})
