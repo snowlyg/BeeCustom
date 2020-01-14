7e886debd84717ead3cabb638d446ca7b8ebe412
luodan@elinkport.com <linximeng>
2020-01-14 09:21:44 +0800

    coding webhook 修改

--- controllers/WebHookController.go
+++ controllers/WebHookController.go
@@ -17,7 +17,7 @@ type WebHookController struct {
 func (c *WebHookController) Get() {
 	signature := c.Ctx.Request.Header.Get("X-Coding-Signature") //获取加密签名
 	//res, err := ioutil.ReadAll(c.Ctx.Request.Body) // for application/json
-	palyload := c.Input().Get("payload")
+	palyload := c.Ctx.Request.PostFormValue("palyload")
 	sha1 := enums.Hmac(SECRETTOKEN, []byte(palyload)) // for application/x-www-form-urlencoded
 	calculateSignature := "sha1=" + sha1              // 重新加密内容
 	utils.LogDebug(fmt.Sprintf("web_hook同步状态: %v - %v ", calculateSignature == signature, palyload))
