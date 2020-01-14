dfac7daef665d804dbaaa073bf33e27d053efea1
luodan@elinkport.com <linximeng>
2020-01-14 09:01:21 +0800

    修改 xml 文件生成方法

--- controllers/WebHookController.go
+++ controllers/WebHookController.go
@@ -19,12 +19,12 @@ func (c *WebHookController) Get() {
 	sha1 := enums.Hmac(SECRETTOKEN, c.Ctx.Input.RequestBody)
 	calculateSignature := "sha1=" + sha1 // 重新加密内容
 	utils.LogDebug(fmt.Sprintf("web_hook同步状态: %v ", calculateSignature == signature))
-	if calculateSignature == signature {
-		enums.Cmd("cd", "", []string{"/root/go/src/BeeCustom"})
-		enums.Cmd("git", "", []string{"pull"})
-		enums.Cmd("go build", "", []string{""})
-		enums.Cmd("supervisorctl", "", []string{"restart", "beepkg"})
-	}
+	//if calculateSignature == signature {
+	enums.Cmd("cd", "", []string{"/root/go/src/BeeCustom"})
+	enums.Cmd("git", "", []string{"pull"})
+	enums.Cmd("go build", "", []string{""})
+	enums.Cmd("supervisorctl", "", []string{"restart", "beepkg"})
+	//}
 
 	//c.Data["json"] = "ok"
 	c.ServeJSON()
