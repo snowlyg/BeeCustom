1532469f348b9e7504532c0b50d4d11872f0ca34
luodan@elinkport.com <linximeng>
2020-01-14 09:17:36 +0800

    coding webhook 修改

--- controllers/WebHookController.go
+++ controllers/WebHookController.go
@@ -17,9 +17,10 @@ type WebHookController struct {
 func (c *WebHookController) Get() {
 	signature := c.Ctx.Request.Header.Get("X-Coding-Signature") //获取加密签名
 	//res, err := ioutil.ReadAll(c.Ctx.Request.Body) // for application/json
-	sha1 := enums.Hmac(SECRETTOKEN, []byte(c.Input().Get("payload"))) // for application/x-www-form-urlencoded
-	calculateSignature := "sha1=" + sha1                              // 重新加密内容
-	utils.LogDebug(fmt.Sprintf("web_hook同步状态: %v ", calculateSignature == signature))
+	palyload := c.Input().Get("payload")
+	sha1 := enums.Hmac(SECRETTOKEN, []byte(palyload)) // for application/x-www-form-urlencoded
+	calculateSignature := "sha1=" + sha1              // 重新加密内容
+	utils.LogDebug(fmt.Sprintf("web_hook同步状态: %v - %v ", calculateSignature == signature, palyload))
 	if calculateSignature == signature {
 		enums.Cmd("cd", "", []string{"/root/go/src/BeeCustom"})
 		enums.Cmd("git", "", []string{"pull"})
